50e4f654257925a93e84e24aa6c3584aa2e35a85
Blueheim <crystalrod0@gmail.com>
2019-01-16 11:24:22 +0100

    remove winston mongo

--- init/logging.js
+++ init/logging.js
@@ -1,17 +1,14 @@
 const winston = require('winston');
-// require('winston-mongodb'); // Can create bug freeze with jest
 require('express-async-errors');
 
 module.exports = function() {
-	winston.exceptions.handle(
-		new winston.transports.Console({ colorize: true, prettyPrint: true }),
-		new winston.transports.File({ filename: 'uncaughtExceptions.log' })
-		//use a process manager to restart for a clean state
-	);
+  winston.exceptions.handle(
+    new winston.transports.Console({ colorize: true, prettyPrint: true }),
+    new winston.transports.File({ filename: 'uncaughtExceptions.log' })
+    //use a process manager to restart for a clean state
+  );
 
-	process.on('unhandledRejection', ex => {
-		throw ex; 
-	});
-
-	//winston.add(new winston.transports.File({ filename: 'logfile.log' }));
+  process.on('unhandledRejection', ex => {
+    throw ex;
+  });
 };
